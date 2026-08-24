cd701d271cfd3d1cb1bbda64dd3c5ccfdd573574
Santiago Beltran Astorga <a225203551@unison.mx>
2026-08-23 18:49:37 -0700

    Completed the implementation for the TiendaApp java file

--- src/com/tienda/app/TiendaApp.java
+++ src/com/tienda/app/TiendaApp.java
@@ -1,16 +1,19 @@
 package com.tienda.app;
 
 import java.util.ArrayList;
-
-import com.tienda.model.Producto;
+import com.tienda.model.*;
 
 public class TiendaApp {
     public static void main(String[] args) {
         ArrayList<Producto> listaProducto = new ArrayList<>();
         Producto laptop = new Producto("P001", "ThinkPad X1", 10000.00, 6);
-
-
+        Producto mouse = new Producto("P002", "Mouse HP", 189.00, 17);
+        Producto teclado = new Producto("P003", "Teclado Razer", 1200.00, 5);
+        
         listaProducto.add(laptop);
+        listaProducto.add(mouse);
+        listaProducto.add(teclado);
+
         for (Producto n : listaProducto) {
             n.mostrarInformacion();
         }
