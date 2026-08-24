4d33ad80815a74252d2b69e68e07f370a292d400
Santiago Beltran Astorga <a225203551@unison.mx>
2026-08-23 18:28:12 -0700

    Implemented TiendaApp java file

--- src/com/tienda/app/TiendaApp.java
+++ src/com/tienda/app/TiendaApp.java
@@ -6,6 +6,13 @@ import com.tienda.model.Producto;
 
 public class TiendaApp {
     public static void main(String[] args) {
-        ArrayList<Producto> producto = new ArrayList<>();
+        ArrayList<Producto> listaProducto = new ArrayList<>();
+        Producto laptop = new Producto("P001", "ThinkPad X1", 10000.00, 6);
+
+
+        listaProducto.add(laptop);
+        for (Producto n : listaProducto) {
+            n.mostrarInformacion();
+        }
     }
 }
